74c660350faee1e262bf75eb717fbe623fd16d27
azizulhakim13 <mah.azizulhakim.sust@gmail.com>
2023-05-24 10:01:10 +0600

    updated sections, added iptv page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 new WOW().init();
+
+$(window).on("scroll", function () {
+    var e = $(window).scrollTop();
+    e > 600 && $(".go-top").addClass("active"), e < 600 && $(".go-top").removeClass("active");
+}),
+$(".go-top").on("click", function () {
+    $("html, body").animate({ scrollTop: "0" }, 500);
+});
